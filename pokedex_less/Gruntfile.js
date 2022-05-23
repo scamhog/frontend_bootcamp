738c12194bf98cc8d697e2ad120fc3042ad20683
scamhog <marcocastrosendoya@gmail.com>
2022-05-23 15:44:46 -0400

    with clear and innited gulp

---
+++ pokedex_less/Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                files: {
+                    'dist/js/pokedex.min.js': 'src/*.js'
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
+};
