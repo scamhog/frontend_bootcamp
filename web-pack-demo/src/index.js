d2fa3890087ea09858440dd596ec398dd2848810
scamhog <marcocastrosendoya@gmail.com>
2022-05-24 18:01:03 -0400

    class challenge webpack

--- web-pack-demo/src/index.js
+++ web-pack-demo/src/index.js
@@ -1,4 +1,13 @@
 import { DateTime } from 'luxon';
+import _ from 'lodash';
 
 const date = DateTime.now().plus({ days: 6 });
 console.log(date.toLocaleString());
+
+function component(){
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'Webpack'],'');
+    return element;
+}
+
+document.body.appendChild(component());
