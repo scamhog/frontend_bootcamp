f835c89f2f91e79aebc80fbbebcb01fd4f58be5c
scamhog <marcocastrosendoya@gmail.com>
2022-05-25 00:06:47 -0400

    grunt ready to work

--- pokedex_less/Gruntfile.js
+++ pokedex_less/Gruntfile.js
@@ -1,17 +1,55 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        package: grunt.file.readJSON('package.json'),
+        clean: ['dist/', 'src/css/'],
         uglify: {
             build: {
+                files: [
+                    {
+                        src: 'src/*.js',
+                        dest: 'dist/js/pokedex.min.js'
+                    }
+                ]
+            }
+        },
+        less: {
+            build: {
+                options: {
+                    paths: ['src/css/']
+                },
                 files: {
-                    'dist/js/pokedex.min.js': 'src/*.js'
+                    'src/css/pokedex.css': 'src/pokedex.less'
                 }
             }
         },
-        clean: {
-            folder: "dist/"
+        cssmin: {
+            target: {
+                files: [
+                    {
+                        src: 'src/css/pokedex.css',
+                        dest: 'dist/css/pokedex.min.css'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            options: {
+                collapseWhitespace: true
+            },
+            target: {
+                files: [
+                    {
+                        src: 'src/index.html',
+                        dest: 'dist/index.html'
+                    }
+                ]
+            }
         }
     });
-    grunt.loadNpmTasks('grunt-clean','grunt-contrib-uglify');
-    grunt.registerTask('default', ['clean','uglify']);
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.registerTask('default', ['clean', 'uglify', 'less', 'cssmin', 'htmlmin']);
 };
