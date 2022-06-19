73929c9b3378d36db8bb0371dd8070ebc1567daf
scamhog <marcocastrosendoya@gmail.com>
2022-06-19 19:25:00 -0400

    previous to create custom directive example

--- tasks-to-do/src/app/app-routing.module.ts
+++ tasks-to-do/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskerComponent } from './pages/tasker/tasker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tasker', component: TaskerComponent },
+  { path: '', component: TaskerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
