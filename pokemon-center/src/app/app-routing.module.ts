b887d9a5d4588ba90f10334c0fad42d581c8ae9c
scamhog <marcocastrosendoya@outlook.com>
2022-06-30 17:14:51 -0400

    routing added x2

--- pokemon-center/src/app/app-routing.module.ts
+++ pokemon-center/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeInfoDetailComponent } from './pokemons/poke-info-detail/poke-info-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'poke-info', component: PokeInfoDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
